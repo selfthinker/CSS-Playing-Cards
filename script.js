46d1a5b046a7dbac336dc80fb4ed6f85837186d6
Anika Henke <anika@selfthinker.org>
2011-06-13 22:17:16 +0100

    improved how the options are handled (JS)

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 $(document).ready(function(){
-    $('#options').addClass('active');
+    // show hint only useful when JS is enabled
     $('.jsHint').show();
+
+    // set class for options to style differently if JS is enabled
+    $('#options').addClass('active');
+
+    var cards = $('.playingCards');
+
+    // toggle options on and off
     $('.toggle li').click(function(){
-        $('.playingCards').toggleClass($(this).text());
+        var thisObj   = $(this);
+        var optionCur = thisObj.text();
+
+        // if option is exclusive, remove other exclusive options
+        switch (optionCur) {
+            case 'faceImages':
+                removeOption('simpleCards');
+                removeOption('inText');
+                break;
+            case 'simpleCards':
+                removeOption('faceImages');
+                removeOption('inText');
+                break;
+            case 'inText':
+                removeOption('faceImages');
+                removeOption('simpleCards');
+                break;
+            default:
+                break;
+        }
+
+        cards.toggleClass(optionCur);
+        thisObj.toggleClass('active');
     });
+
+    var removeOption = function(option) {
+        $('.toggle li').each(function(){
+            var thisObj   = $(this);
+            var optionCur = thisObj.text();
+            if (optionCur==option){
+                thisObj.removeClass('active');
+                cards.removeClass(option);
+            }
+        });
+    };
+
+    // set different document language
     $('.lang li').click(function(){
-        $('html').attr('lang', $(this).text());
-        $('html').attr('xml:lang', $(this).text());
+        var thisObj = $(this);
+        var lang    = thisObj.text();
+
+        $('html').attr('lang', lang);
+        $('html').attr('xml:lang', lang);
+
+        thisObj.siblings().removeClass('active');
+        thisObj.addClass('active');
+
     });
+
+    // initiate jQueryUI tabs
     $('#cards').tabs();
 });
